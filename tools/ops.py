c3bed2a23e67acefdfe9d6ff2621f56ee90ba75c
KungYao <masa10269@gmail.com>
2022-01-11 23:23:13 +0800

    adjust loss

--- tools/ops.py
+++ tools/ops.py
@@ -46,7 +46,7 @@ def compute_pt_regression_loss(predict_contours, predict_regressions, target_con
             losses.append(loss_p2t + loss_t2p)
         else:
             losses.append(p_regress.sum() * 0)
-    losses = torch.mean(torch.stack(losses))
+    losses = torch.sum(torch.stack(losses))
     return losses
 
 def compute_hinge_loss(logit, mode):
