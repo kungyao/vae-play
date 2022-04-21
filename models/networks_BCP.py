bf949ba5806d88af3fe37ef1623eb2920634eb45
KungYao <masa10269@gmail.com>
2022-04-22 00:34:54 +0800

    add train_BCP and networks_BCP

---
+++ models/networks_BCP.py
@@ -0,0 +1,135 @@
+from copy import deepcopy
+import numpy as np
+import torch
+import torch.nn as nn
+
+from torch.nn.functional import grid_sample
+from torchvision.models.resnet import resnet50
+from torchvision.models.densenet import densenet121
+
+try:
+    from models.blocks import *
+except:
+    from blocks import *
+from tools.utils import find_contour, resample_points
+
+VALUE_WEIGHT = 10
+
+class ContentEndoer(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.convs = nn.Sequential(
+            Conv2d(in_channels, 64, 3, stride=2, bn=None, activate="lrelu"), 
+            Conv2d(64, 128, 3, stride=2, bn=None, activate="lrelu"), 
+            Conv2d(128, 256, 3, stride=2, bn=None, activate="lrelu"), 
+            Conv2d(256, 512, 3, stride=2, bn=None, activate="lrelu"), 
+            Conv2d(512, 1024, 3, stride=2, bn=None, activate="lrelu"), 
+            Conv2d(1024, 2048, 3, stride=1, bn=None, activate="lrelu"), 
+            Conv2d(2048, 2048, 3, stride=1, bn=None, activate="lrelu"), 
+        )
+        self.out_channels = 2048
+
+    def forward(self, x: torch.Tensor):
+        x = self.convs(x)
+        return x
+
+class LinePredictor(nn.Module):
+    def __init__(self, image_size, pt_size=4096, in_channels=256):
+        super().__init__()
+        self.max_point = pt_size
+        self.batch_attention = nn.Sequential(
+            SelfAttentionBlock(pt_size), 
+            SelfAttentionBlock(pt_size), 
+            SelfAttentionBlock(pt_size)
+        )
+        # -10~10
+        # -10~10
+        # offset_x, offset_y
+        self.params_pred = nn.Sequential(
+            Linear(in_channels, in_channels, activate='lrelu'), 
+            Linear(in_channels, in_channels, activate=None), 
+            Linear(in_channels, 2, activate=None)
+        )
+
+    def process(self, x: torch.Tensor, contours: torch.Tensor):
+        dtype = x.dtype
+        device = x.device
+        b, c, h, w = x.shape
+        resamples = []
+        for i, cnt in enumerate(contours):
+            normalized_pts = cnt.to(device)
+            if normalized_pts.numel() != 0:
+                normalized_pts = normalized_pts.unsqueeze(0).unsqueeze(0)
+                resample = grid_sample(x[i][None], normalized_pts, mode='bicubic')
+                resample = resample.squeeze()
+                resample = resample.permute(1, 0)
+                if self.max_point > resample.size(0):
+                    resample_padding = torch.zeros(self.max_point - resample.size(0), c, dtype=dtype, device=device)
+                    resample = torch.cat([resample, resample_padding], dim=0)
+            else:
+                resample = torch.zeros(self.max_point, c, dtype=dtype, device=device)
+            resamples.append(resample)
+        resamples = torch.stack(resamples, dim=0)
+        return resamples
+    
+    def forward(self, x: torch.Tensor, contours: torch.Tensor):
+        # Sample points on feature map. (batch, pt, in_channel(256)).
+        x = self.process(x, contours) 
+        # Do predict.
+        b, c, n = x.shape
+        x = x.reshape(b, c, n, 1)
+        x = self.batch_attention(x)
+        x = x.reshape(b, c, n)
+        size_per_img = [len(x) for x in contours]
+        tmp_x = []
+        for i in range(b):
+            tmp_x.append(x[i][:size_per_img[i]])
+        x = torch.cat(tmp_x, dim=0)
+        # 
+        x = self.params_pred(x) 
+        return x
+
+class ComposeNet(nn.Module):
+    def __init__(self, image_size, pt_size=4096):
+        super(ComposeNet, self).__init__()
+        self.max_point = pt_size
+        # 實心or射線
+        self.cls_classifier = nn.Sequential(
+            resnet50(pretrained=True), 
+            nn.Linear(1000, 2)
+        )
+
+        self.add_coord = AddCoords(if_normalize=True)
+        self.encoder = ContentEndoer(3 + 2)
+        self.line_predictor = LinePredictor(image_size, pt_size=4096, in_channels=self.encoder.out_channels)
+
+    def forward(self, x, target=None):
+        if self.training:
+            size = []
+            contours = []
+            for t in target:
+                size.append(len(t["total"]))
+                contours.append(t["total"][:, :2])
+            # contours = torch.stack(contours, dim=0)
+        else:
+            b = x.shape[0]
+            size = []
+            contours = []
+            for i in range(b):
+                cnt = find_contour(b[i])
+                cnt = resample_points(cnt, self.max_point)
+                size.append(len(cnt))
+                contours.append(cnt)
+            # contours = torch.stack(contours, dim=0)
+        # 
+        cls_x = self.cls_classifier(x)
+        # 
+        x = self.add_coord(x)
+        x = self.encoder(x)
+        x = self.line_predictor(x, contours)
+        x = x.split(size)
+        return {
+            "classes": cls_x, 
+            "contours": contours, 
+            "target_pts": x, 
+        }
---
+++ train_BCP.py
@@ -0,0 +1,203 @@
+import os
+import argparse
+from datetime import datetime
+
+import cv2
+import torch
+import torchvision.utils as vutils
+import torchvision.transforms.functional as TF
+from tqdm import tqdm, trange
+from torch.utils.data import DataLoader
+
+from datasets .dataset import BCPDataset
+from models.networks_BCP import ComposeNet
+# from test_BP import save_test_batch
+from tools.ops import *
+from tools.utils import makedirs
+
+def train_collate_fn(batch):
+    imgs, bmasks, labels, annotation = zip(*batch)
+    imgs = torch.stack(imgs, dim=0)
+    bmasks = torch.stack(bmasks, dim=0)
+    labels = torch.tensor(labels)
+    return imgs, bmasks, labels, annotation
+
+def train(args, epoch, iterations, net, optim, train_loader):
+    net.train()
+    
+    count = 0
+    avg_loss = {
+        "loss_class": 0, 
+        "loss_total_regress": 0, 
+        "loss_key_regress": 0, 
+    }
+
+    train_iter = iter(train_loader)
+    for i in trange(iterations):
+        try:
+            imgs, bmasks, labels, annotation = next(train_iter)
+        except:
+            train_iter = iter(train_loader)
+            imgs, bmasks, labels, annotation = next(train_iter)
+
+        b = imgs.size(0)
+        imgs = imgs.cuda(args.gpu)
+        # bmasks = bmasks.cuda(args.gpu)
+        labels = labels.cuda(args.gpu)
+        annotation = [{k: v.cuda(args.gpu) for k, v in t.items()} for t in annotation]
+
+        preds = net(imgs, target=annotation)
+
+        size = [len(t["total"]) for t in annotation]
+        key_contour = [t["key"] for t in annotation]
+        total_contour = torch.cat([t["total"][:, 2:4] for t in annotation], dim=0) * VALUE_WEIGHT
+
+        loss_class = F.cross_entropy(preds["classes"], labels)
+        loss_total_regress = F.l1_loss(
+            torch.cat(preds["target_pts"], dim=0), 
+            total_contour
+        )
+
+        loss_key_regress = []
+        for cnt_idx, key in enumerate(key_contour):
+            if key.numel() != 0:
+                loss_key_regress.append(
+                    F.l1_loss(preds["target_pts"][cnt_idx][key[:, 4].to(torch.long)], key[:, 2:4] * VALUE_WEIGHT)
+                )
+        if len(loss_key_regress) != 0:
+            loss_key_regress = torch.mean(torch.stack(loss_key_regress, dim=0))
+        else:
+            loss_key_regress = torch.tensor(0)
+
+        losses = loss_class + loss_total_regress + loss_key_regress * 10
+
+        optim.zero_grad()
+        losses.backward()
+        optim.step()
+
+        with torch.no_grad():
+            next_count = count + imgs.size(0)
+            avg_loss["loss_class"] = (avg_loss["loss_class"] * count + loss_class.item()) / next_count
+            avg_loss["loss_total_regress"] = (avg_loss["loss_total_regress"] * count + loss_total_regress.item()) / next_count
+            avg_loss["loss_key_regress"] = (avg_loss["loss_key_regress"] * count + loss_key_regress.item()) / next_count
+            count = next_count
+
+            if (i+1) % args.viz_freq == 0:
+                print("")
+                res_str = f"[Epoch: {epoch}]。"
+                for key in avg_loss:
+                    res_str += f"{key}: {round(avg_loss[key], 6)}; "
+                print(res_str)
+                imgs = imgs.cpu()
+                _, classes = torch.max(preds["classes"], dim=1)
+                split_preds_target_pts = [x.cpu() for x in preds["target_pts"]]
+                save_test_batch(imgs, bmasks, classes, preds["contours"], split_preds_target_pts, args.res_output, f"{epoch}_{i+1}")
+    return
+
+def save_test_batch(imgs, bmasks, classes, contours, contour_targets, result_path, result_name):
+    b, c, h, w = imgs.shape
+    
+    results = []
+    for i in range(b):
+        tmp_b = bmasks[i].clone()
+        tmp_b = torch.permute(tmp_b, (1, 2, 0))
+        tmp_b = np.ascontiguousarray(tmp_b.numpy()).astype(np.uint8) * 255
+        cnt = (contours[i] / VALUE_WEIGHT * 0.5 + 0.5) * h
+        cnt_target = (contour_targets[i] / VALUE_WEIGHT * 0.5 + 0.5) * h
+        cnt_size = len(cnt)
+        if classes[i] == 1:
+            for j in range(cnt_size):
+                pt = cnt[j].to(dtype=torch.long).tolist()
+                end_pt = cnt_target[j].to(dtype=torch.long).tolist()
+                cv2.line(tmp_b, (pt[0], pt[1]), (end_pt[0], end_pt[1]), (255, 255, 255), thickness=1)
+        else:
+            for j in range(cnt_size):
+                pt = cnt_target[j].to(dtype=torch.long).tolist()
+                end_pt = cnt_target[(j+1)%cnt_size].to(dtype=torch.long).tolist()
+                cv2.line(tmp_b, (pt[0], pt[1]), (end_pt[0], end_pt[1]), (255, 255, 255), thickness=1)
+        tmp_b = TF.to_tensor(tmp_b)
+        results.append(tmp_b)
+    results = torch.stack(results, dim=0)
+    vutils.save_image(
+        torch.cat([imgs, bmasks, results], dim=0), 
+        os.path.join(result_path, f"{result_name}.png"),
+        nrow=b, 
+        padding=2, 
+        pad_value=1
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--path', type=str, dest='path', default="D:/Manga/bubble-cnt-data")
+    # 
+    parser.add_argument('--lr', type=float, dest='lr', default=1e-3)
+    parser.add_argument('--gpu', type=int, dest='gpu', default=0)
+    parser.add_argument('--epoch', type=int, dest='epochs', default=1)
+    parser.add_argument('--iterations', type=int, dest='iterations', default=200)
+    parser.add_argument('--batchsize', type=int, dest='batchsize', default=8)
+    #
+    parser.add_argument('--workers', type=int, dest='workers', default=0)
+    # 
+    parser.add_argument('--img_size', type=int, dest='img_size', default=512)
+    # parser.add_argument('--max_points', type=int, dest='max_points', default=DEFAULT_MAX_POINTS)
+    #
+    parser.add_argument('--res_output', type=str, dest='res_output', default='./results')
+    parser.add_argument('--model_output', type=str, dest='model_output', default='./logs')
+    parser.add_argument('--viz_freq', type=int, dest='viz_freq', default=10)
+    args = parser.parse_args()
+
+    dest_name = os.path.join("BCP", datetime.now().strftime("%Y%m%d-%H%M%S"))
+    args.res_output = os.path.join(args.res_output, dest_name)
+    args.model_output = os.path.join(args.model_output, dest_name)
+
+    makedirs(args.res_output)
+    makedirs(args.model_output)
+
+    record_txt = open(os.path.join(args.model_output, "record.txt"), "w")
+    for arg in vars(args):
+        record_txt.write('{:35}{:20}\n'.format(arg, str(getattr(args, arg))))
+    record_txt.close()
+
+    # width height
+    dset = BCPDataset(args.path, args.img_size)
+    dloader = DataLoader(
+        dset, 
+        batch_size=args.batchsize, 
+        shuffle=True, 
+        num_workers=args.workers, 
+        collate_fn=train_collate_fn, 
+        pin_memory=True)
+    
+    net = ComposeNet(args.img_size)
+    initialize_model(net.encoder)
+    initialize_model(net.line_predictor)
+
+    net.cuda(args.gpu)
+
+    optim = torch.optim.Adam(net.parameters(), lr=args.lr)
+    step_size = 2
+    # step_size = args.epochs // 4
+    step_size = 1 if step_size == 0 else step_size
+    scheduler = torch.optim.lr_scheduler.StepLR(optim, step_size, gamma=0.1)
+
+    for epoch in range(args.epochs):
+        train(args, epoch, args.iterations, net, optim, dloader)
+        torch.save(
+            {
+                "networks": net, 
+                # "optims": optim,
+                "epoch": epoch
+            }, 
+            os.path.join(args.model_output, f"{epoch}.ckpt")
+        )
+        scheduler.step()
+
+    # for i, (imgs, bmasks, labels, annotations) in enumerate(dloader):
+    #     b, c, h, w = imgs.shape
+    #     contours = []
+    #     contour_targets = []
+    #     for x in annotations:
+    #         print(len(x["total"]))
+    #         contours.append(x["total"][:, :2] * VALUE_WEIGHT)
+    #         contour_targets.append(x["total"][:, 2:4] * VALUE_WEIGHT)
+    #     save_test_batch(imgs, bmasks, labels, contours, contour_targets, result_path="./results", result_name=f"test_{i}")
