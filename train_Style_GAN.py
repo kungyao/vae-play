92e4a33a9ba754d98ebd9cd7633ac81f9efb8093
KungYao <masa10269@gmail.com>
2022-07-09 20:04:48 +0800

    update output folder for Style GAN

--- train_Style_GAN.py
+++ train_Style_GAN.py
@@ -296,7 +296,7 @@ if __name__ == "__main__":
     parser.add_argument('--viz_freq', type=int, dest='viz_freq', default=100)
     args = parser.parse_args()
 
-    dest_name = os.path.join("BE_GAN", datetime.now().strftime("%Y%m%d-%H%M%S"))
+    dest_name = os.path.join("Style_GAN", datetime.now().strftime("%Y%m%d-%H%M%S"))
     args.res_output = os.path.join(args.res_output, dest_name)
     args.model_output = os.path.join(args.model_output, dest_name)
 
