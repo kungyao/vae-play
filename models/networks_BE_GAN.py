7e7ca08af794e4d5cb99bf00b6768005999a1acb
KungYao <masa10269@gmail.com>
2022-07-10 14:18:38 +0800

    remove specific encoder (because that's wierd) and update new discriminator and model

--- models/networks_BE_GAN.py
+++ models/networks_BE_GAN.py
@@ -1,4 +1,5 @@
 
+from tkinter.tix import Tree
 import torch
 import torch.nn as nn
 import numpy as np
@@ -7,64 +8,6 @@ try:
     from models.blocks import *
 except:
     from blocks import *
-
-class PartialEncoder(nn.Module):
-    def __init__(self, in_channels, in_size, latent_length):
-        super().__init__()
-        max_channel = latent_length
-
-        # 
-        self.convs = []
-        repeat_num = int(np.log2(in_size)) - 2
-        self.convs = [Conv2d(in_channels, 32, 3, stride=1, bn="instance")]
-        in_channels = 32
-        out_channels = min(in_channels * 2, max_channel)
-        for _ in range(repeat_num): 
-            self.convs.append(Conv2d(in_channels, out_channels, 3, stride=2, bn="instance"))
-            in_channels = out_channels
-            out_channels = min(in_channels * 2, max_channel)
-        self.convs.append(Conv2d(in_channels, max_channel, 1, stride=1, bn="instance"))
-        self.convs.append(nn.AdaptiveAvgPool2d((1, 1)))
-        self.convs = nn.Sequential(*self.convs)
-
-        # 
-        self.linears = nn.Sequential(
-            Linear(max_channel, latent_length, bias=True, activate='relu'), 
-            Linear(latent_length, latent_length, bias=True, activate='relu'), 
-            Linear(latent_length, latent_length, bias=False, activate=None)
-        )
-    
-    def forward(self, x: torch.Tensor):
-        # 
-        x = self.convs(x)
-        x = x.reshape(x.size(0), -1)
-        x = self.linears(x)
-        return x
-
-class LatentMapper(nn.Module):
-    def __init__(self, latent_size, target_size):
-        super().__init__()
-        latent_length = latent_size * latent_size
-        if_upsample = target_size > latent_size
-        if_donw_sample = target_size < latent_size
-        repeat_num = int(np.log2(max(latent_size, target_size) // min(latent_size, target_size)))
-        self.convs = []
-        for _ in range(repeat_num):
-            if if_upsample:
-                self.convs.append(nn.Upsample(scale_factor=2, mode='bilinear'))
-            self.convs.append(Conv2d(1, 1, 3, 2 if if_donw_sample else 1, bn="instance"))
-        for _ in range(3):
-            self.convs.append(Conv2d(1, 1, 3, 1, bn="instance"))
-
-        self.linears = nn.Sequential(Linear(latent_length, latent_length), Linear(latent_length, latent_length))
-        self.convs = nn.Sequential(*self.convs)
-        self.latent_size = latent_size
-    
-    def forward(self, x):
-        x = self.linears(x)
-        x = x.reshape(x.size(0), 1, self.latent_size, self.latent_size)
-        x = self.convs(x)
-        return x
 
 class MaskNet(nn.Module):
     def __init__(self, in_channel):
@@ -89,23 +32,17 @@ class EdgeNet(MaskNet):
         return x
 
 class ComposeNet(nn.Module):
-    def __init__(self, in_channels, in_size, latent_size):
+    def __init__(self, in_channels, in_size):
         super().__init__()
         # Feature extract
-        min_channel = 16
-        max_channel = 32
+        min_channel = 32
+        max_channel = 256
         min_in_size = int(np.power(2, 4))
         repeat_num = int(np.log2(in_size // min_in_size))
         
         # 
-        embed_type_count = 2
-        latent_length = latent_size * latent_size
-        up_size_list = []
-        # 
-
         self.down = nn.ModuleList()
         self.down.append(Conv2d(3, min_channel, 3, stride=1, bn="batch"))
-        cur_size = in_size
         in_channels = min_channel
         out_channels = min(in_channels * 2, max_channel)
         for _ in range(repeat_num):
@@ -115,9 +52,6 @@ class ComposeNet(nn.Module):
             ))
             in_channels = out_channels
             out_channels = min(in_channels * 2, max_channel)
-            # 
-            cur_size = cur_size // 2
-            up_size_list.append(cur_size)
 
         # 
         self.up = nn.ModuleList()
@@ -126,51 +60,18 @@ class ComposeNet(nn.Module):
         in_channels = min_channel
         out_channels = min(in_channels * 2, max_channel)
         for _ in range(repeat_num):
-            self.up.append(Up(out_channels + embed_type_count, in_channels))
+            self.up.append(Up(out_channels, in_channels))
             self.skip.append(Conv2d(in_channels, in_channels, 3, stride=1, bn="instance"))
             self.cat.append(Conv2d(in_channels * 2, in_channels, 3, stride=1, bn="instance"))
             in_channels = out_channels
             out_channels = min(in_channels * 2, max_channel)
 
-        # 
-        self.content_encoder = PartialEncoder(3, in_size, latent_length)
-        self.boundary_encoder = PartialEncoder(3, in_size, latent_length)
-
-        self.content_code_size_mapping = nn.ModuleList()
-        self.boundary_code_size_mapping = nn.ModuleList()
-        for up_size in up_size_list:
-            self.content_code_size_mapping.append(LatentMapper(latent_size, up_size))
-            self.boundary_code_size_mapping.append(LatentMapper(latent_size, up_size))
-        
         # Generate content mask
         self.mask_net = MaskNet(min_channel)
         # Generate edge mask
         self.edge_net = EdgeNet(min_channel)
 
-    def forward_latent(self, x: torch.Tensor, y=None):
-        if y is None:
-            content_code = self.content_encoder(x)
-            boundary_code = self.boundary_encoder(x)
-        else:
-            # 
-            c_mask = y[:, 0, :, :][:, None].repeat(1, 3, 1, 1)
-            b_mask = y[:, 1, :, :][:, None].repeat(1, 3, 1, 1)
-            # 
-            content_code = self.content_encoder(c_mask)
-            boundary_code = self.boundary_encoder(1.0 - b_mask)
-        return content_code, boundary_code
-    
-    def forward_content_latent(self, x: torch.Tensor):
-        content_code = self.content_encoder(x)
-        return content_code
-
-    def forward_boundary_latent(self, x: torch.Tensor):
-        boundary_code = self.boundary_encoder(x)
-        return boundary_code
-
-    def forward(self, x: torch.Tensor, y=None):
-        # 
-        content_code, boundary_code = self.forward_latent(x, y=y)
+    def forward(self, x: torch.Tensor):
         # 
         down_feats = []
         for m in self.down:
@@ -179,11 +80,6 @@ class ComposeNet(nn.Module):
         # 
         for i in range(len(self.up)):
             idx = len(self.up) - 1 - i
-            # Cat with latent code
-            level_content_code = self.content_code_size_mapping[idx](content_code)
-            level_boundary_code = self.boundary_code_size_mapping[idx](boundary_code)
-            
-            x = torch.cat([x, level_content_code, level_boundary_code], dim=1)
             # Up-sampling
             x_up = self.up[idx](x)
             x_skip = self.skip[idx](down_feats[len(down_feats) - 2 - i])
@@ -201,10 +97,9 @@ class ComposeNet(nn.Module):
         }
         return output
 
-class Discriminator(nn.Module):
-    def __init__(self, in_channels, in_size):
+class MaskMapper(nn.Module):
+    def __init__(self, in_channels, in_size, max_channel=128):
         super().__init__()
-        max_channel = 64
         min_in_size = int(np.power(2, 4))
         repeat_num = int(np.log2(in_size // min_in_size))
 
@@ -219,17 +114,45 @@ class Discriminator(nn.Module):
             self.feat_modules .append(Conv2d(in_channels, out_channels, 3, 2, bn='batch', activate='lrelu'))
             in_channels = out_channels
             out_channels = min(in_channels * 2, max_channel)
-
-    def forward(self, x: torch.Tensor, y: torch.Tensor):
-        # 
+        self.pooler = nn.Sequential(
+            Conv2d(in_channels, max_channel, 1, 1, bn=None, activate=None), 
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+    
+    def forward(self, x: torch.Tensor, m: torch.Tensor):
         x = x[:, 0, :, :].reshape(x.size(0), 1, x.size(2), x.size(3))
-        x = torch.cat([x, y], dim=1)
+        x = torch.cat([x, m], dim=1)
         # 
         x = self.convs(x)
-        # 
         feat_list = []
         for idx, m in enumerate(self.feat_modules):
             x = m(x)
             feat_list.append(x.reshape(x.size(0), -1) * (idx // 2 + 1))
-        x = torch.cat(feat_list, dim=1)
-        return x
+        # 
+        feat_list = torch.cat(feat_list, dim=1)
+        x = self.pooler(x)
+        x = x.reshape(x.size(0), -1)
+        return x, feat_list
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, in_size, num_classes):
+        super().__init__()
+        max_channel = 128
+        self.num_classes = num_classes
+        self.content_disc = MaskMapper(2, in_size, max_channel=max_channel)
+        self.boundary_disc = MaskMapper(2, in_size, max_channel=max_channel)
+    
+        self.predictor = nn.Sequential(
+            Linear(max_channel*2, max_channel*2, bias=True, activate='lrelu'), 
+            Linear(max_channel*2, max_channel, bias=True, activate='lrelu'), 
+            Linear(max_channel, num_classes, bias=False, activate=None), 
+        )
+
+    def forward(self, x: torch.Tensor, m1: torch.Tensor, m2: torch.Tensor):
+        x_m1, feats_m1 = self.content_disc(x, m1)
+        x_m2, feats_m2 = self.boundary_disc(x, m2)
+        # 
+        feats = torch.cat([feats_m1, feats_m2], dim=1)
+        x = torch.cat([x_m1, x_m2], dim=1)
+        x = self.predictor(x)
+        return x, feats
--- train_BE_GAN.py
+++ train_BE_GAN.py
@@ -28,8 +28,6 @@ def train(args, epoch, iterations, nets, optims, train_loader):
     D = nets["D"]
 
     g_opt = optims["G"]
-    g_opt_style_c = optims["Style_C"]
-    g_opt_style_b = optims["Style_B"]
     d_opt = optims["D"]
 
     G.train()
@@ -38,9 +36,11 @@ def train(args, epoch, iterations, nets, optims, train_loader):
     count = 0
     avg_loss = {
         "d_adv_loss": 0,
+        "d_type_loss": 0,
         "loss_edge": 0,
         "loss_mask": 0,
         "g_adv_loss": 0,
+        "g_type_loss": 0,
         "loss_style_content": 0,
         "loss_style_boundary": 0,
     }
@@ -61,61 +61,48 @@ def train(args, epoch, iterations, nets, optims, train_loader):
         # Boundary only image
         eimgs = eimgs.cuda(args.gpu)
         labels = labels.cuda(args.gpu)
-        merged_mask = torch.cat([bimgs, eimgs], dim=1)
 
         # D
         with torch.no_grad():
-            preds = G(imgs, y=merged_mask)
+            preds = G(imgs)
             pred_masks = preds["masks"]
             pred_edges = preds["edges"]
-        d_fake = D(imgs, torch.cat([pred_masks, pred_edges], dim=1))
-        d_real = D(imgs, merged_mask)
-        d_adv_loss = 1 - torch.mean(torch.abs(d_fake - d_real))
+        d_real_type, d_real_feats = D(imgs, bimgs, eimgs)
+        d_fake_type, d_fake_feats = D(imgs, pred_masks, pred_edges)
+
+        d_adv_loss = 1 - torch.mean(torch.abs(d_fake_feats - d_real_feats))
+        d_type_loss = F.cross_entropy(d_real_type, labels)
+        d_losses = d_adv_loss + d_type_loss
 
         d_opt.zero_grad()
-        d_adv_loss.backward()
+        d_losses.backward()
         d_opt.step()
 
         # G - GT
-        preds = G(imgs, y=merged_mask)
+        preds = G(imgs)
         pred_masks = preds["masks"]
         pred_edges = preds["edges"]
 
-        g_real = D(imgs, merged_mask)
-        g_pred = D(imgs, torch.cat([pred_masks, pred_edges], dim=1))
+        _, g_real_feats = D(imgs, bimgs, eimgs)
+        g_pred_type, g_pred_feats = D(imgs, pred_masks, pred_edges)
 
         loss_mask = 0.5 * F.binary_cross_entropy_with_logits(pred_masks, bimgs) + compute_dice_loss(pred_masks.sigmoid(), bimgs)
         loss_egde = 0.5 * F.binary_cross_entropy_with_logits(pred_edges, eimgs) + compute_dice_loss(pred_edges.sigmoid(), eimgs)
-        g_adv_loss = torch.mean(torch.abs(g_pred - g_real))
-        losses = loss_mask + loss_egde + g_adv_loss
+        g_adv_loss = torch.mean(torch.abs(g_pred_feats - g_real_feats))
+        g_type_loss = F.cross_entropy(g_pred_type, labels)
+        losses = loss_mask + loss_egde + g_adv_loss + g_type_loss
 
         g_opt.zero_grad()
         losses.backward()
         g_opt.step()
-
-        # G - Style
-        with torch.no_grad():
-            c_style_gt, b_style_gt = G.forward_latent(imgs, y=merged_mask)
-        c_style_pred, b_style_pred = G.forward_latent(imgs)
-
-        loss_style_content = F.l1_loss(c_style_pred, c_style_gt)
-        g_opt_style_c.zero_grad()
-        loss_style_content.backward()
-        g_opt_style_c.step()
-
-        loss_style_boundary = F.l1_loss(b_style_pred, b_style_gt)
-        g_opt_style_b.zero_grad()
-        loss_style_boundary.backward()
-        g_opt_style_b.step()
-
         # 
         next_count = count + imgs.size(0)
         avg_loss["d_adv_loss"] = (avg_loss["d_adv_loss"] * count + d_adv_loss.item()) / next_count
+        avg_loss["d_type_loss"] = (avg_loss["d_type_loss"] * count + d_type_loss.item()) / next_count
         avg_loss["loss_edge"] = (avg_loss["loss_edge"] * count + loss_egde.item()) / next_count
         avg_loss["loss_mask"] = (avg_loss["loss_mask"] * count + loss_mask.item()) / next_count
         avg_loss["g_adv_loss"] = (avg_loss["g_adv_loss"] * count + g_adv_loss.item()) / next_count
-        avg_loss["loss_style_content"] = (avg_loss["loss_style_content"] * count + loss_style_content.item()) / next_count
-        avg_loss["loss_style_boundary"] = (avg_loss["loss_style_boundary"] * count + loss_style_boundary.item()) / next_count
+        avg_loss["g_type_loss"] = (avg_loss["g_type_loss"] * count + g_adv_loss.item()) / next_count
         count = next_count
 
         if (i+1) % args.viz_freq == 0:
@@ -126,16 +113,8 @@ def train(args, epoch, iterations, nets, optims, train_loader):
             print(res_str)
             with torch.no_grad():
                 # with gt mask
-                preds = G(imgs, y=torch.cat([bimgs, eimgs], dim=1))
+                preds = G(imgs)
                 save_test_batch(imgs, preds, args.res_output, f"{epoch}_{i+1}_wgtm")
-                # no gt mask
-                preds = G(imgs, y=None)
-                save_test_batch(imgs, preds, args.res_output, f"{epoch}_{i+1}_ngtm")
-                # no mask
-                zero_mask = torch.zeros(imgs.size(0), 2, imgs.size(2), imgs.size(3))
-                zero_mask = zero_mask.cuda(args.gpu)
-                preds = G(imgs, y=zero_mask)
-                save_test_batch(imgs, preds, args.res_output, f"{epoch}_{i+1}_nembed")
     return
 
 if __name__ == "__main__":
@@ -170,8 +149,9 @@ if __name__ == "__main__":
         record_txt.write('{:35}{:20}\n'.format(arg, str(getattr(args, arg))))
     record_txt.close()
 
-    generator = ComposeNet(3, args.img_size, args.z_size)
-    discriminator = Discriminator(3, args.img_size)
+    generator = ComposeNet(3, args.img_size)
+    # unknown, oval, explode, emit
+    discriminator = Discriminator(3, args.img_size, 4)
 
     initialize_model(generator)
     initialize_model(discriminator)
@@ -182,8 +162,6 @@ if __name__ == "__main__":
 
     optims = {}
     optims["G"] = torch.optim.Adam(generator.parameters(), lr=args.lr)
-    optims["Style_C"] = torch.optim.Adam(generator.content_encoder.parameters(), lr=args.lr)
-    optims["Style_B"] = torch.optim.Adam(generator.boundary_encoder.parameters(), lr=args.lr)
     optims["D"] = torch.optim.Adam(discriminator.parameters(), lr=args.lr)
 
     for name, net in nets.items():
